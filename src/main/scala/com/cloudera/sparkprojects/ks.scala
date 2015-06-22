5a2b0479c5921840b728aa6eb22ab86f22028009
jose.cambronero <jose.cambronero@cloudera.com>
2015-06-22 13:09:50 -0700

    fixed up some errors and added a more efficient testOneSample, that creates only 1 distribution object per partition

--- src/main/scala/com/cloudera/sparkprojects/ks.scala
+++ src/main/scala/com/cloudera/sparkprojects/ks.scala
@@ -10,31 +10,50 @@ import org.apache.spark.rdd.RDD
 import Math._
 
 object KS {
-  def ecdf(dat: RDD[Double]): RDD[Double] = {
-    val n = dat.count
-    dat.sortBy(x => x).zipWithIndex().map { case (v, i) => i / n }
+  def ecdf(dat: RDD[Double]): RDD[(Double, Double)] = {
+    val n = dat.count().toDouble
+    dat.sortBy(x => x).zipWithIndex().map { case (v, i) => (v, i / n) }
   }
 
-  // KS test for 1 set of sample data, versus theoretical, using string for name
-  // of distribution
-  def testOneSample(dat: RDD[Double], dist: String): (Double, Double) = {
-    val cdf = 
-      dist match {
-        case "stdnorm" => (x: Double) => new NormalDistribution().cumulativeProbability(x)
-        case  _ =>  throw new UnsupportedOperationException()
-    }
-    testOneSample(dat, cdf)
-  }
-  
   // KS test for 1 set of sample data (versus theoretical distribution)
   // 2-sided variant
   def testOneSample(dat: RDD[Double], cdf: Double => Double): (Double, Double) = {
     val empiriRDD = ecdf(dat) // empirical distribution
     val distances = empiriRDD.map { case (v, empVal) => Math.abs(cdf(v) -  empVal) }
     val ksStat = distances.max
-    evalOneSided(ksStat, distances.length)
+    evalOneSided(ksStat, distances.count())
   }
-  
+
+  // more efficient implementation for partitions
+  def testOneSample(dat: RDD[Double], partOp: Iterator[(Double, Double)] => Iterator[Double])
+    : (Double, Double) = {
+    val empiriRDD = ecdf(dat) // empirical distribution
+    val distances = empiriRDD.mapPartitions(partOp, false)
+    val ksStat = distances.max
+    evalOneSided(ksStat, distances.count())
+  }
+
+  // Create only 1 distribution object per partition (rather than 1 per observation)
+  def stdNormMap(): (Iterator[(Double, Double)]) => Iterator[Double] = {
+    val dist = new NormalDistribution(0, 1)
+    (part: Iterator[(Double, Double)]) => part.map {
+      case (v, empVal) => Math.abs(dist.cumulativeProbability(v) - empVal)
+    }
+  }
+
+  // KS test for 1 set of sample data, versus theoretical, using string for name
+  // of distribution
+  def testOneSample(dat: RDD[Double], distName: String): (Double, Double) = {
+    val cdf =
+      distName match {
+        case "stdnorm" => stdNormMap()
+        case  _ =>  throw new UnsupportedOperationException()
+      }
+
+    testOneSample(dat, cdf)
+  }
+
+
   def testTwoSample(dat1: RDD[Double], dat2: RDD[Double]): (Double, Double) = { 
     if(dat1.count != dat2.count) {
       // throw exception for mismatched lengths (search mllib for existing exception)
@@ -42,13 +61,13 @@ object KS {
     }
     val empiri1 = ecdf(dat1)
     val empiri2 = ecdf(dat2)
-    val distances = empiri1.zip(empiri2).map { case (p1, p2) => Math.abs(p1 - p2) }
+    val distances = empiri1.zip(empiri2).map { case (p1, p2) => Math.abs(p1._2 - p2._2) }
     val ksStat = distances.max
-    evalOneSided(ksStat, distances.length)
+    evalOneSided(ksStat, distances.count())
   }
   
-  private[KS] def evalOneSided(ksstat: Double, n: Int): Double = {
-    val pval = 1 - new KolmogorovSmirnovTest().cdf(ksStat, n)
+  private[KS] def evalOneSided(ksStat: Double, n: Long): (Double, Double) = {
+    val pval = 1 - new KolmogorovSmirnovTest().cdf(ksStat, n.toInt)
     (ksStat, pval)
   }
 
